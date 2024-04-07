8ac6aef830f7e8c1acbe83ba66c82a3a639ed858
Manish <mannyinwork@gmail.com>
2024-04-07 17:40:38 +0530

    javaScript

--- VariablesAndDataTypes/first.js
+++ VariablesAndDataTypes/first.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+//  let a = 15
+// console.log(a);
+
+// const student = {
+//     fullName : "Rahul Kumar",
+//     age : 21,
+//     cgpa : 9.1,
+//     isPass : true
+// };
+
+// const profile = {
+//     userId : "shradhakhapra",
+//     isFollow : true,
+//     message : "message",
+//     posts : 195,
+//     followers : 569,
+//     following : 4,
+//     userName : "Shradha Khapra",
+//     thread : "shradhakhapra",
+//     bioTitle : "Entrepreneur",
+//     bio : "Apna College | Ex-Microsoft, DRDO",
+//     bioSec : "To educate someone is the highest privilege"
+// };
+
+// function create (name, num) {
+//     console.log(name);
+//     console.log(num);
+// }
+
+// create ("manish", 555)
